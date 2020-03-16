adcc45e41a2896e7d7f0fdb3351032c49767534c
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-16 23:48:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,19 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
+function greet(timeString) {
+  const houe = parseInt(timeString, 10)
+  if (hour < 12) {
+    greeting = "Good Morning"
+  }
+
+  else if (hour > 17) {
+    greeting = "Good Evening"
+  }
+
+  else {
+    greeting = "Good Afternoon"
+  }
+  return greeting
+}
 /* Write your implementation of displayMessage() */
